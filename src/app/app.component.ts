02972fc0e8da20de7fef0cc93bd1b7448b8d47d1
DURAND <aurelie.durand@capgemini.com>
2018-10-16 11:48:50 +0200

    code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Post } from './models/Post.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'blog-angular';
+  posts = [
+  	new Post(
+  		"Mon premier post",
+  		"Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+  		2),
+  	new Post(
+  		"Mon deuxième post",
+  		"Nunc iaculis orci in pellentesque maximus. Duis in posuere leo. Vivamus ac lectus massa. Etiam eleifend cursus metus id scelerisque. Sed mollis in dolor sit amet venenatis. Mauris sollicitudin ligula sed libero suscipit blandit. Integer non dui consequat, sollicitudin arcu non, faucibus felis. Pellentesque convallis risus auctor dui aliquam consectetur.",
+  		-1),
+  	new Post(
+  		"Encore un post",
+  		"Suspendisse eu sollicitudin ante. Cras et scelerisque eros, et aliquam velit. Praesent eget orci porta, commodo lectus sed, auctor velit. Vivamus rhoncus ante purus. Sed mi enim, pharetra non enim ac, mattis lobortis purus. Quisque scelerisque velit vel libero convallis sollicitudin. Cras ornare tincidunt elementum. Donec suscipit suscipit vestibulum.",
+  		0)
+  ];
 }
---
+++ src/app/post-list/post-list.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-post-list',
+  templateUrl: './post-list.component.html',
+  styleUrls: ['./post-list.component.css']
+})
+export class PostListComponent implements OnInit {
+
+	@Input() postTitle: string;
+	@Input() postContent: string;
+	@Input() postLoveIts: number;
+	@Input() postDate: Date;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  getTitle() {
+  	return this.postTitle;
+  }
+
+  getContent() {
+  	return this.postContent;
+  }
+
+  getDate() {
+  	return this.postDate;
+  }
+
+  onLoveIt() {
+  	this.postLoveIts++;
+  }
+
+  onDontLoveIt() {
+  	this.postLoveIts--;
+  }
+}
